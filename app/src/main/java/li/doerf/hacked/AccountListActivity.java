703b91eac5805b1fbc66f1f8cefd43c97c0a8e72
Marc Doerflinger <marc@doerf.li>
2016-09-07 23:06:37 +0200

    show initial help

--- app/src/main/java/li/doerf/hacked/AccountListActivity.java
+++ app/src/main/java/li/doerf/hacked/AccountListActivity.java
@@ -1,18 +1,22 @@
 package li.doerf.hacked;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import li.doerf.hacked.db.DatasetChangeListener;
@@ -54,6 +58,26 @@ public class AccountListActivity extends AppCompatActivity implements DatasetCha
         LinearLayoutManager lm = new LinearLayoutManager(getApplicationContext());
         accountsList.setLayoutManager(lm);
         accountsList.setAdapter(myAccountsAdapter);
+
+        final SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        boolean initialHelpDismissed = settings.getBoolean(getString(R.string.pref_initial_help_dismissed), false);
+
+        if ( ! initialHelpDismissed ) {
+            final CardView initialHelp = (CardView) findViewById(R.id.initial_help);
+            initialHelp.setVisibility(View.VISIBLE);
+            Button dismissB = (Button) findViewById(R.id.button_dismiss_help);
+            dismissB.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    initialHelp.setVisibility(View.GONE);
+                    Toast.makeText(getApplicationContext(), getString(R.string.toast_dont_show_initial_help_again), Toast.LENGTH_SHORT).show();
+
+                    SharedPreferences.Editor editor = settings.edit();
+                    editor.putBoolean(getString(R.string.pref_initial_help_dismissed), true);
+                    editor.apply();
+                }
+            });
+        }
     }
 
 
