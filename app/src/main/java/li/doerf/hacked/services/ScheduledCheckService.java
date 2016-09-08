610b6bc16e1220c32143a5f37d1be82deb17f7d1
Marc Doerflinger <marc@doerf.li>
2016-09-08 23:43:08 +0200

    schedule check service

--- app/src/main/java/li/doerf/hacked/services/ScheduledCheckService.java
+++ app/src/main/java/li/doerf/hacked/services/ScheduledCheckService.java
@@ -2,7 +2,11 @@ package li.doerf.hacked.services;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
+
+import li.doerf.hacked.R;
 
 /**
  * Created by moo on 08/09/16.
@@ -15,17 +19,48 @@ public class ScheduledCheckService extends IntentService {
         super("ScheduledCheckService");
     }
 
-    /**
-     * Creates an IntentService.  Invoked by your subclass's constructor.
-     *
-     * @param name Used to name the worker thread, important only for debugging.
-     */
-    public ScheduledCheckService(String name) {
-        super(name);
-    }
-
     @Override
     protected void onHandleIntent(Intent intent) {
         Log.d(LOGTAG, "onHandleIntent");
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        long lastSync = settings.getLong(getString(R.string.PREF_KEY_LAST_SYNC_TIMESTAMP), 0);
+        int currentInterval = getCurrentInterval(settings);
+
+        // check if time has come to run the service
+        if ( System.currentTimeMillis() < lastSync + currentInterval ) {
+            return;
+        }
+
+        Log.i(LOGTAG, "check interval expired. run check now");
+        Intent i = new Intent(this, HaveIBeenPwnedCheckService.class);
+        startService(i);
+
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putLong(getString(R.string.PREF_KEY_LAST_SYNC_TIMESTAMP), System.currentTimeMillis());
+        editor.apply();
+    }
+
+    private int getCurrentInterval(SharedPreferences aSettings) {
+        String intervalString = aSettings.getString(getString(R.string.pref_key_sync_interval), "everyday");
+
+        switch ( intervalString) {
+            case "everyday":
+                return 1000 * 30;
+            // TODO reenable after testing
+//                return 1000 * 60 * 60 * 24;
+
+            case "everytwodays":
+                return 1000 * 60 * 60 * 24 * 2;
+
+            case "everythreedays":
+                return 1000 * 60 * 60 * 24 * 3;
+
+            case "everyweek":
+                return 1000 * 60 * 60 * 24 * 7;
+
+            default:
+                return 1000 * 60 * 60 * 24;
+        }
     }
 }
