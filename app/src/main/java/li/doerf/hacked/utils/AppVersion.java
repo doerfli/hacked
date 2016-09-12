a3237388d5c46ac246300398b30b3495231706db
Marc Doerflinger <marc@doerf.li>
2016-09-12 10:23:15 +0200

    add appversion object

---
+++ app/src/main/java/li/doerf/hacked/utils/AppVersion.java
@@ -0,0 +1,23 @@
+package li.doerf.hacked.utils;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+/**
+ * Created by moo on 12/09/16.
+ */
+public class AppVersion {
+    private final String LOGTAG = "AppVersion";
+
+    public static String getAppVersion( Context aContext) {
+        try {
+            PackageInfo packageInfo = aContext.getPackageManager().getPackageInfo(aContext.getPackageName(), 0);
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.w(LOGTAG, "exception while retrieving version", e);
+        }
+        return "Unknown";
+    }
+}
