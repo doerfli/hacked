3b7cb7c4b532d9bfbdcb7189244664f88696f870
Marc Doerflinger <github@doerf.li>
2018-09-26 21:52:02 +0200

    remove todo

--- app/src/main/java/li/doerf/hacked/ui/fragments/BreachDetailsFragment.java
+++ app/src/main/java/li/doerf/hacked/ui/fragments/BreachDetailsFragment.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -30,8 +30,7 @@ import li.doerf.hacked.utils.BackgroundTaskHelper;
  * Created by moo on 06/10/16.
  */
 public class BreachDetailsFragment extends Fragment {
-    private final String LOGTAG = getClass().getSimpleName();
-//    private Account myAccount;
+//    private final String LOGTAG = getClass().getSimpleName();
     private BreachesAdapter myBreachesAdapter;
     private long myAccountId;
     private BreachViewModel myViewModel;
@@ -42,46 +41,43 @@ public class BreachDetailsFragment extends Fragment {
         return fragment;
     }
 
-
     @SuppressLint("CheckResult")
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        AccountDao accountDao = AppDatabase.get(context).getAccountDao();
         myViewModel = ViewModelProviders.of(this).get(BreachViewModel.class);
         myBreachesAdapter = new BreachesAdapter(getContext(), new ArrayList<>());
-
-        new BackgroundTaskHelper<Account>().runInBackgroundAndConsumeOnMain(() -> accountDao.findById(myAccountId), account -> {
-            getActivity().setTitle(account.getName());
-            myViewModel.getBreachList(
-                    account.getId()).observe(
-                    BreachDetailsFragment.this, accounts -> myBreachesAdapter.addItems(accounts));
-        });
     }
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_breach_details, container, false);
+        CardView noBreachFound = view.findViewById(R.id.no_breach_found);
 
-        // TODO do something about this
-//        if ( myBreachesAdapter.getItemCount() == 0 ) {
-//            CardView noBreachFound = (CardView) view.findViewById(R.id.no_breach_found);
-//            noBreachFound.setVisibility(View.VISIBLE);
-//            TextView hibpInfo = (TextView) view.findViewById(R.id.hibp_info);
-//            hibpInfo.setVisibility(View.GONE);
-//            return view;
-//        }
+        AccountDao accountDao = AppDatabase.get(getContext()).getAccountDao();
+        new BackgroundTaskHelper<Account>().runInBackgroundAndConsumeOnMain(() -> accountDao.findById(myAccountId), account -> {
+            getActivity().setTitle(account.getName());
+            myViewModel.getBreachList(
+                    account.getId()).observe(
+                    BreachDetailsFragment.this, accounts -> {
+                        myBreachesAdapter.addItems(accounts);
+                        if ( myBreachesAdapter.getItemCount() == 0 ) {
+                            noBreachFound.setVisibility(View.VISIBLE);
+                        } else {
+                            noBreachFound.setVisibility(View.GONE);
+                        }
+                    });
+        });
 
-        RecyclerView breachesList = (RecyclerView) view.findViewById(R.id.breaches_list);
+        RecyclerView breachesList = view.findViewById(R.id.breaches_list);
         breachesList.setHasFixedSize(true);
         LinearLayoutManager lm = new LinearLayoutManager(getContext());
         breachesList.setLayoutManager(lm);
         breachesList.setAdapter(myBreachesAdapter);
-
-        HibpInfo.prepare( getContext(), (TextView) view.findViewById(R.id.hibp_info), breachesList);
-
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(breachesList.getContext(),
                 lm.getOrientation());
         breachesList.addItemDecoration(dividerItemDecoration);
+
+        HibpInfo.prepare( getContext(), view.findViewById(R.id.hibp_info), breachesList);
 
         return view;
     }
