87be543fdf29c59a2ce41bfd0b94ff8bf1130c87
Marc Doerflinger <github@doerf.li>
2019-12-12 22:07:16 +0100

    enable tls for android 4.4 and below

--- app/src/main/java/li/doerf/hacked/HackedApplication.java
+++ app/src/main/java/li/doerf/hacked/HackedApplication.java
@@ -1,5 +1,6 @@
 package li.doerf.hacked;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -13,6 +14,7 @@ import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ProcessLifecycleOwner;
 import androidx.multidex.MultiDexApplication;
 
+import com.google.android.gms.security.ProviderInstaller;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 import java.util.List;
@@ -37,10 +39,23 @@ public class HackedApplication extends MultiDexApplication implements LifecycleO
     @Override
     public void onCreate() {
         super.onCreate();
+        enableTls12ForAndroidApi19AndBelow();
         ProcessLifecycleOwner.get().getLifecycle().addObserver(this);
         firebaseAnalytics = FirebaseAnalytics.getInstance(this);
         migrateBackgroundCheckService();
         migrateNumBreaches();
+    }
+
+    private void enableTls12ForAndroidApi19AndBelow() {
+        ProviderInstaller.installIfNeededAsync(this, new ProviderInstaller.ProviderInstallListener() {
+            @Override
+            public void onProviderInstalled() {
+            }
+            @Override
+            public void onProviderInstallFailed(int i, Intent intent) {
+                Log.i(TAG, "Provider install failed (" + i + ") : SSL Problems may occurs");
+            }
+        });
     }
 
     public synchronized void trackView(String name) {
