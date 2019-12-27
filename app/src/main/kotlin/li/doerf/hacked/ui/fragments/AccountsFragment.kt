4bf513d30d4fa243727ffde5690d13fd95d51971
Marc Doerflinger <github@doerf.li>
2019-12-27 14:36:35 +0100

    cleanup

--- app/src/main/kotlin/li/doerf/hacked/ui/fragments/AccountsFragment.kt
+++ app/src/main/kotlin/li/doerf/hacked/ui/fragments/AccountsFragment.kt
@@ -17,29 +17,21 @@ import li.doerf.hacked.ui.adapters.AccountsAdapter
 import li.doerf.hacked.ui.viewmodels.AccountViewModel
 import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [AccountsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class AccountsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
     private lateinit var accountsAdapter: AccountsAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+//    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -68,19 +60,17 @@ class AccountsFragment : Fragment() {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment AccountsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                AccountsFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+        fun newInstance() = AccountsFragment()
+//        fun newInstance(param1: String, param2: String) =
+//                AccountsFragment().apply {
+//                    arguments = Bundle().apply {
+//                        putString(ARG_PARAM1, param1)
+//                        putString(ARG_PARAM2, param2)
+//                    }
+//                }
     }
 }
--- app/src/main/kotlin/li/doerf/hacked/ui/fragments/BreachesFragment.kt
+++ app/src/main/kotlin/li/doerf/hacked/ui/fragments/BreachesFragment.kt
@@ -22,11 +22,6 @@ import li.doerf.hacked.ui.adapters.BreachedSitesAdapter
 import li.doerf.hacked.ui.viewmodels.BreachedSitesViewModel
 import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [BreachesFragment.newInstance] factory method to
@@ -34,17 +29,6 @@ private const val ARG_PARAM2 = "param2"
  */
 class BreachesFragment : Fragment() {
     private lateinit var breachedSitesAdapter: BreachedSitesAdapter
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -87,19 +71,10 @@ class BreachesFragment : Fragment() {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment BreachesFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                BreachesFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+        fun newInstance() = BreachesFragment()
     }
 }
