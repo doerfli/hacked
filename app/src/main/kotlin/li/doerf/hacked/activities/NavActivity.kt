92e0ba9b4ac37f314ae0357436a06d214ec021bc
Marc Doerflinger <github@doerf.li>
2020-01-14 22:01:34 +0100

    add toolbar with menu

--- app/src/main/kotlin/li/doerf/hacked/activities/NavActivity.kt
+++ app/src/main/kotlin/li/doerf/hacked/activities/NavActivity.kt
@@ -1,7 +1,9 @@
 package li.doerf.hacked.activities
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import li.doerf.hacked.R
 
 
@@ -10,6 +12,15 @@ class NavActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nav)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.title = getString(R.string.app_name)
+        title = getString(R.string.app_name)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_overview, menu)
+        return true
     }
 
 }
