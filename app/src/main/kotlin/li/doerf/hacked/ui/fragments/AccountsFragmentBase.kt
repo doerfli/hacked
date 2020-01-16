6461010a11b5e7801e3326117a84457317a26515
Marc Doerflinger <github@doerf.li>
2020-01-16 22:57:13 +0100

    add account at bottom of accounts list

--- app/src/main/kotlin/li/doerf/hacked/ui/fragments/AccountsFragmentBase.kt
+++ app/src/main/kotlin/li/doerf/hacked/ui/fragments/AccountsFragmentBase.kt
@@ -1,24 +1,35 @@
 package li.doerf.hacked.ui.fragments
 
 
+import android.app.Application
+import android.content.ContentValues
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.Group
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.work.*
+import io.reactivex.Single
+import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.*
+import li.doerf.hacked.CustomEvent
+import li.doerf.hacked.HackedApplication
 import li.doerf.hacked.R
 import li.doerf.hacked.db.AppDatabase
 import li.doerf.hacked.db.daos.AccountDao
 import li.doerf.hacked.db.entities.Account
+import li.doerf.hacked.remote.haveibeenpwned.HIBPAccountCheckerWorker
 import li.doerf.hacked.ui.adapters.AccountsAdapter
 import li.doerf.hacked.ui.viewmodels.AccountViewModel
 import org.joda.time.format.DateTimeFormat
@@ -30,19 +41,13 @@ import java.util.*
  * create an instance of this fragment.
  */
 abstract class AccountsFragmentBase : Fragment(), NavDirectionsToAccountDetailsFactory {
+    private lateinit var groupAddAccount: Group
     private lateinit var accountDao: AccountDao
     private lateinit var accountsAdapter: AccountsAdapter
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-//    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
         accountDao = AppDatabase.get(context).accountDao
     }
 
@@ -82,6 +87,22 @@ abstract class AccountsFragmentBase : Fragment(), NavDirectionsToAccountDetailsF
             fragmentRootView.findNavController().navigate(action)
         }
 
+        val account = fragmentRootView.findViewById<EditText>(R.id.account)
+
+        val addButton = fragmentRootView.findViewById<Button>(R.id.add)
+        addButton.setOnClickListener {
+            val accountName = account.text
+            addAccount(accountName.toString())
+            groupAddAccount.visibility = View.GONE
+        }
+
+        val cancelButton = fragmentRootView.findViewById<Button>(R.id.cancel)
+        cancelButton.setOnClickListener {
+            groupAddAccount.visibility = View.GONE
+        }
+
+        groupAddAccount = fragmentRootView.findViewById(R.id.group_add_account)
+
         return fragmentRootView
     }
 
@@ -90,6 +111,67 @@ abstract class AccountsFragmentBase : Fragment(), NavDirectionsToAccountDetailsF
         accountsAdapter = AccountsAdapter(context, ArrayList(), this)
         val accountsViewModel = ViewModelProviders.of(this).get(AccountViewModel::class.java)
         accountsViewModel.accountList.observe(this, Observer { accounts: List<Account> -> accountsAdapter.addItems(accounts) })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_fragment_accounts, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_add) {
+            groupAddAccount.visibility = View.VISIBLE
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun addAccount(aName: String) {
+        if (aName == null || aName.trim { it <= ' ' } == "") {
+            Toast.makeText(context, getString(R.string.toast_enter_valid_name), Toast.LENGTH_LONG).show()
+            Log.w(LOGTAG, "account name not valid")
+            return
+        }
+        val name = aName.trim { it <= ' ' }
+        val accountDao = AppDatabase.get(context).accountDao
+        getAccountCount(name, accountDao)
+                .subscribe(Consumer { count: Int ->
+                    if (count == 0) {
+                        val account = Account()
+                        account.name = name
+                        account.numBreaches = 0
+                        account.numAcknowledgedBreaches = 0
+                        insertAccount(accountDao, account, activity!!.application)
+                    }
+
+                })
+    }
+
+    private fun getAccountCount(name: String, accountDao: AccountDao): Single<Int> {
+        return Single.fromCallable { accountDao.countByName(name) }
+                .subscribeOn(Schedulers.io())
+    }
+
+    private fun insertAccount(accountDao: AccountDao, account: Account, application: Application) {
+        Single.fromCallable { accountDao.insert(account) }
+                .subscribeOn(Schedulers.io())
+                .subscribe({ ids: List<Long> ->
+                    (application as HackedApplication).trackCustomEvent(CustomEvent.ACCOUNT_ADDED)
+                    val inputData = Data.Builder()
+                            .putLong(HIBPAccountCheckerWorker.KEY_ID, ids[0])
+                            .build()
+                    val constraints = Constraints.Builder()
+                            .setRequiredNetworkType(NetworkType.UNMETERED)
+                            .build()
+                    val checker = OneTimeWorkRequest.Builder(HIBPAccountCheckerWorker::class.java)
+                            .setInputData(inputData)
+                            .setConstraints(constraints)
+                            .build()
+                    WorkManager.getInstance().enqueue(checker)
+                }) { throwable: Throwable? -> Log.e(ContentValues.TAG, "Error msg", throwable) }
+    }
+
+    companion object {
+        const val LOGTAG = "AccountsFragmentBase"
     }
 
 }
