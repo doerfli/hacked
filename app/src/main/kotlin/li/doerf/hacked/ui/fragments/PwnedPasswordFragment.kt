fed3ade71345114f84b0896834dbef7186c52362
Marc Doerflinger <github@doerf.li>
2020-01-21 22:57:56 +0100

    cleanup

--- app/src/main/kotlin/li/doerf/hacked/ui/fragments/PwnedPasswordFragment.kt
+++ app/src/main/kotlin/li/doerf/hacked/ui/fragments/PwnedPasswordFragment.kt
@@ -10,8 +10,6 @@ import li.doerf.hacked.R
 
 /**
  * A simple [Fragment] subclass.
- * Use the [PwnedPasswordFragment.newInstance] factory method to
- * create an instance of this fragment.
  */
 class PwnedPasswordFragment : Fragment() {
 
@@ -21,15 +19,4 @@ class PwnedPasswordFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_pwned_password, container, false)
     }
 
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         * @return A new instance of fragment PwnedPasswordFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) = PwnedPasswordFragment()
-    }
 }
