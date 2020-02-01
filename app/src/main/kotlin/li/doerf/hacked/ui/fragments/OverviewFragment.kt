387e8b469837d8b2cd0458573fab858e28eef839
Marc Doerflinger <github@doerf.li>
2020-02-01 14:01:34 +0100

    show hibp info for 5 seconds

--- app/src/main/kotlin/li/doerf/hacked/ui/fragments/OverviewFragment.kt
+++ app/src/main/kotlin/li/doerf/hacked/ui/fragments/OverviewFragment.kt
@@ -1,10 +1,14 @@
 package li.doerf.hacked.ui.fragments
 
 import android.os.Bundle
+import android.text.Html
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import kotlinx.coroutines.*
 import li.doerf.hacked.R
 
 /**
@@ -17,10 +21,31 @@ import li.doerf.hacked.R
  */
 class OverviewFragment : Fragment() {
 
+    private lateinit var hibpInfo: TextView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overview, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_overview, container, false)
+
+        hibpInfo = rootView.findViewById<TextView>(R.id.hibp_info)
+        hibpInfo.movementMethod = LinkMovementMethod.getInstance()
+        hibpInfo.text = Html.fromHtml("${getString(R.string.data_provided_by)} <a href=\"https://haveibeenpwned.com\">Have i been pwned?</a>")
+
+        return rootView
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        hibpInfo.visibility = View.VISIBLE
+
+        GlobalScope.launch {
+            delay(5000)
+            withContext(Dispatchers.Main) {
+                hibpInfo.visibility = View.GONE
+            }
+        }
     }
 
 }
