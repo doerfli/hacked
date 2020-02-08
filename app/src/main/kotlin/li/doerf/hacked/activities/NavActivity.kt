5778a350c40ba812839530809adf7fec227efa95
Marc Doerflinger <github@doerf.li>
2020-02-08 15:05:15 +0100

    remove unused resources

--- app/src/main/kotlin/li/doerf/hacked/activities/NavActivity.kt
+++ app/src/main/kotlin/li/doerf/hacked/activities/NavActivity.kt
@@ -33,7 +33,7 @@ class NavActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean { // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.menu_nav, menu)
         return true
     }
 
