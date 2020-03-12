f353afc2a7ca4bb346f0d46a41fb977989c2c44f
Marc Doerflinger <github@doerf.li>
2020-03-12 22:41:52 +0100

    implement breach filter (fixes #87)

--- app/src/main/java/li/doerf/hacked/ui/viewmodels/BreachedSitesViewModel.java
+++ app/src/main/java/li/doerf/hacked/ui/viewmodels/BreachedSitesViewModel.java
@@ -2,11 +2,14 @@ package li.doerf.hacked.ui.viewmodels;
 
 import android.app.Application;
 
-import java.util.List;
-
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+
+import java.util.List;
+
 import li.doerf.hacked.db.AppDatabase;
 import li.doerf.hacked.db.daos.BreachedSiteDao;
 import li.doerf.hacked.db.entities.BreachedSite;
@@ -17,10 +20,19 @@ public class BreachedSitesViewModel extends AndroidViewModel {
     private LiveData<List<BreachedSite>> myBreachedSites;
     private LiveData<List<BreachedSite>> myBreachedSitesTop20;
     private LiveData<List<BreachedSite>> myBreachedSitesMostRecent;
+    private MutableLiveData<String> filterLiveData = new MutableLiveData<>("");
 
     public BreachedSitesViewModel(@NonNull Application application) {
         super(application);
         myBreachedSitesDao = AppDatabase.get(application).getBrachedSiteDao();
+        myBreachedSites = Transformations.switchMap(filterLiveData,
+                v -> {
+                    if (v == null || v.trim().equals("")) {
+                        return myBreachedSitesDao.getAllLD();
+                    } else {
+                        return myBreachedSitesDao.getAllByName("%" + v + "%");
+                    }
+                });
     }
 
     public LiveData<List<BreachedSite>> getBreachesSites() {
@@ -28,6 +40,10 @@ public class BreachedSitesViewModel extends AndroidViewModel {
             myBreachedSites = myBreachedSitesDao.getAllLD();
         }
         return myBreachedSites;
+    }
+
+    public void setFilter(String filter) {
+        filterLiveData.setValue(filter);
     }
 
     public LiveData<List<BreachedSite>> getBreachesSitesTop20() {
