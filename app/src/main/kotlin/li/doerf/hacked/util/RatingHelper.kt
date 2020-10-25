9c518c8f3619aaaef57b7f909bfbfdcadc535abf
Marc Doerflinger <marc@doerf.li>
2020-10-25 14:29:13 +0100

    use review api (#139)

--- app/src/main/kotlin/li/doerf/hacked/util/RatingHelper.kt
+++ app/src/main/kotlin/li/doerf/hacked/util/RatingHelper.kt
@@ -3,26 +3,57 @@ package li.doerf.hacked.util
 import android.app.Activity
 import android.preference.PreferenceManager
 import android.util.Log
-import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentManager
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import li.doerf.hacked.ui.RateUsDialogFragment
+import com.google.android.play.core.review.ReviewManagerFactory
+import li.doerf.hacked.CustomEvent
+import li.doerf.hacked.HackedApplication
 
 class RatingHelper(private val activity: Activity) {
     private val LOGTAG = javaClass.simpleName
 
     private suspend fun showRateUsDialog() {
         // since the show is delayed (and app could be closed now) this needs to be checked here
-        if (!activity.isFinishing && !activity.isDestroyed) {
-            withContext(Dispatchers.Main) {
-                val dialog = RateUsDialogFragment()
-                val fragmentManager = (activity as FragmentActivity).supportFragmentManager as FragmentManager
-                if (! fragmentManager.isDestroyed && !fragmentManager.isStateSaved) {
-                    dialog.show(fragmentManager, "rateus")
+//        if (!activity.isFinishing && !activity.isDestroyed) {
+//            withContext(Dispatchers.Main) {
+//                val dialog = RateUsDialogFragment()
+//                val fragmentManager = (activity as FragmentActivity).supportFragmentManager as FragmentManager
+//                if (! fragmentManager.isDestroyed && !fragmentManager.isStateSaved) {
+//                    dialog.show(fragmentManager, "rateus")
+//                }
+//            }
+//        }
+        val manager = ReviewManagerFactory.create(activity.applicationContext)
+//        val manager = FakeReviewManager(activity.applicationContext)
+        val request = manager.requestReviewFlow()
+        Log.d(LOGTAG, "requesting review flow")
+        request.addOnCompleteListener { requestr ->
+            if (requestr.isSuccessful) {
+                Log.d(LOGTAG, "review flow request successful")
+                // We got the ReviewInfo object
+                val reviewInfo = requestr.result
+                val flow = manager.launchReviewFlow(activity, reviewInfo)
+                Log.d(LOGTAG, "launching review flow")
+                flow.addOnCompleteListener { _ ->
+                    // The flow has finished. The API does not indicate whether the user
+                    // reviewed or not, or even whether the review dialog was shown. Thus, no
+                    // matter the result, we continue our app flow.
+                    Log.i(LOGTAG, "review finished")
+                    saveSettingRated()
                 }
+
+            } else {
+                Log.e(LOGTAG, "unable to request review flow", request.exception)
             }
         }
+
+    }
+
+    private fun saveSettingRated() {
+        val settings = PreferenceManager.getDefaultSharedPreferences(activity.applicationContext)
+        val editor = settings.edit()
+        editor.putBoolean(PREF_KEY_HAS_RATED_US, true)
+        editor.apply()
+        (activity.getApplication() as HackedApplication).trackCustomEvent(CustomEvent.RATE_NOW)
+        Log.i(LOGTAG, "setting: rated us - true")
     }
 
     private fun showNoRatingDialog(): Boolean {
